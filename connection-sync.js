94d4ed8df0dccd458f6e9288b06caba54a75e954
Silas Vergilio <silas.vergilio@gmail.com>
2022-05-25 00:10:24 -0300

    fix/db sync

--- connection-sync.js
+++ connection-sync.js
@@ -1,13 +1,14 @@
 var mysql = require('sync-mysql');
+var dbConfig = require('./dbconfig');
 
 require('dotenv').config();
 
 var connection = new mysql({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    database : "ftcawards",
+    host     : dbConfig.HOST,
+    user     : dbConfig.USER,
+    password : dbConfig.PASSWORD,
+   // port: process.env.DB_PORT,
+    database : dbConfig.DB,
   });
 
   module.exports = connection;
