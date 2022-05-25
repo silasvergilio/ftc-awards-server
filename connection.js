4f36b3ffbea828173ff8f80407a73831cd68129d
Silas Vergilio <silas.vergilio@gmail.com>
2022-05-24 23:21:19 -0300

    fix/host db

--- connection.js
+++ connection.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 var dbConfig = require('./dbconfig');
 
 var connection = mysql.createConnection({
-    host     : dbConfig.HOST,
-    user     : dbConfig.USER,
-    password : dbConfig.PASSWORD,
-    //port: process.env.DB_PORT,
-    database : dbConfig.DB,
-  });
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  //port: process.env.DB_PORT,
+  database: dbConfig.DB,
+});
 
-  connection.connect((err) => {
-    if (err) throw err;
-  })
-  
-  module.exports = connection;
+connection.connect((err) => {
+  if (err) throw err;
+})
+
+module.exports = connection;
--- dbconfig.js
+++ dbconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "106a10a0@us-cdbr-east-05.cleardb.net",
+    HOST: "us-cdbr-east-05.cleardb.net",
     USER: "bc231dc796d45c",
     PASSWORD: "106a10a0",
     DB: "heroku_cf8b471fbab3885"
