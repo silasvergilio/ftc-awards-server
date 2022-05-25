2fe09ed5a33168b3d4188ff181823348597c24b7
Silas Vergilio <silas.vergilio@gmail.com>
2022-05-25 15:31:04 -0300

    fix/cors origin

--- app.js
+++ app.js
@@ -37,19 +37,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(cors({
- // origin: ".herokuapp.com",
+  origin: "https://spa-ftc-awards.herokuapp.com",
   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
   credentials: true,
   
 }));
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 app.use(session({
   secret: "AUSHhu",
